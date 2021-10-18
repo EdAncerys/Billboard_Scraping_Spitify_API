c04e2ba142528091d6f461649cf143623f7382e1
Ed <lookatemail@gmail.com>
2021-10-18 20:42:44 +0100

    Able to fetch JWT from Spotify

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
-print(os.environ['Client_ID'])
-print(os.environ['Client_Secret'])
+
+# env variables fro Spotify API
+Client_ID = os.environ['Client_ID']
+Client_Secret = os.environ['Client_Secret']
+print('Client_ID: ' + str(Client_ID))
+print('Client_Secret: ' + str(Client_Secret))
 
 # date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 date = '2003-09-01'
@@ -17,3 +21,15 @@ song_names_spans = soup.find_all("span", class_="chart-element__information__son
 song_names = [song.getText() for song in song_names_spans]
 print(song_names)
 
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com/callback?code=AQAgOZS3XICJOYg_MYZCVBJCUCD50Lj8u-g2TD-6foVm2WKzcr5_9vhDsYUzfpFYefigrffBLZkmGZZdpsjuFEqzQeQ2GqkOsi6tn-BUhvVThBwGN7fEHPlhi66Ted2wNjBR6BIzk0Lt_ci6ZCQscgoUVRziOLFYn2Wrv8Yqbj6EYq0JPxAjevcWupGmOQasm1hjrp1kQMA",
+        client_id=Client_ID,
+        client_secret=Client_Secret,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
