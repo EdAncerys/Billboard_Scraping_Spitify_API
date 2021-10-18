76c5a9928e64b3770b8a20f4ae21095a64f55427
Ed <lookatemail@gmail.com>
2021-10-18 20:12:10 +0100

    Add API keys to env from spotify

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+print(os.environ['Client_ID'])
+print(os.environ['Client_Secret'])
 
 # date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 date = '2003-09-01'
@@ -11,3 +16,4 @@ soup = BeautifulSoup(response.text, 'html.parser')
 song_names_spans = soup.find_all("span", class_="chart-element__information__song")
 song_names = [song.getText() for song in song_names_spans]
 print(song_names)
+
